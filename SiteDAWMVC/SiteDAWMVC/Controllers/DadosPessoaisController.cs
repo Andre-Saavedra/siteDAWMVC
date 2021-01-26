ce8bd256311f69921b793a5767d5aa6feb784a70
andre saavedra <andre.saavedra@hotmail.com>
2021-01-26 11:07:21 +0000

    criei o controller e a view

---
+++ SiteDAWMVC/SiteDAWMVC/Controllers/DadosPessoaisController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SiteDAWMVC.Data;
+using SiteDAWMVC.Models;
+
+namespace SiteDAWMVC.Controllers
+{
+    public class DadosPessoaisController : Controller
+    {
+        private readonly SiteDbContext _context;
+
+        public DadosPessoaisController(SiteDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: DadosPessoais
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.DadosPessoais.ToListAsync());
+        }
+
+        // GET: DadosPessoais/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var dadosPessoais = await _context.DadosPessoais
+                .FirstOrDefaultAsync(m => m.DadosPessoaisId == id);
+            if (dadosPessoais == null)
+            {
+                return NotFound();
+            }
+
+            return View(dadosPessoais);
+        }
+
+        // GET: DadosPessoais/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: DadosPessoais/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("DadosPessoaisId,Nome,DataNascimento,Telemovel,Email")] DadosPessoais dadosPessoais)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(dadosPessoais);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(dadosPessoais);
+        }
+
+        // GET: DadosPessoais/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var dadosPessoais = await _context.DadosPessoais.FindAsync(id);
+            if (dadosPessoais == null)
+            {
+                return NotFound();
+            }
+            return View(dadosPessoais);
+        }
+
+        // POST: DadosPessoais/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("DadosPessoaisId,Nome,DataNascimento,Telemovel,Email")] DadosPessoais dadosPessoais)
+        {
+            if (id != dadosPessoais.DadosPessoaisId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(dadosPessoais);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!DadosPessoaisExists(dadosPessoais.DadosPessoaisId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(dadosPessoais);
+        }
+
+        // GET: DadosPessoais/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var dadosPessoais = await _context.DadosPessoais
+                .FirstOrDefaultAsync(m => m.DadosPessoaisId == id);
+            if (dadosPessoais == null)
+            {
+                return NotFound();
+            }
+
+            return View(dadosPessoais);
+        }
+
+        // POST: DadosPessoais/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var dadosPessoais = await _context.DadosPessoais.FindAsync(id);
+            _context.DadosPessoais.Remove(dadosPessoais);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool DadosPessoaisExists(int id)
+        {
+            return _context.DadosPessoais.Any(e => e.DadosPessoaisId == id);
+        }
+    }
+}
--- SiteDAWMVC/SiteDAWMVC/Migrations/20210126014332_appDb.Designer.cs
+++ SiteDAWMVC/SiteDAWMVC/Migrations/20210126014332_appDb.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SiteDAWMVC.Data;
 
 namespace SiteDAWMVC.Migrations
 {
     [DbContext(typeof(SiteDbContext))]
-    partial class SiteDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210126014332_appDb")]
+    partial class appDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
