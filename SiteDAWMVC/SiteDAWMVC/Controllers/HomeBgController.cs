d3f492d5a8c22b4e1c1a8afe418cd5bdc3f5ed32
andre saavedra <andre.saavedra@hotmail.com>
2021-02-04 16:52:24 +0000

    inseri imagem na home page

--- SiteDAWMVC/SiteDAWMVC/Controllers/HomeBgController.cs
+++ SiteDAWMVC/SiteDAWMVC/Controllers/HomeBgController.cs
@@ -31,6 +31,7 @@ namespace SiteDAWMVC.Controllers
             return View(await _context.HomeBg.ToListAsync());
         }
 
+
         // GET: HomeBg/Details/5
         public async Task<IActionResult> Details(int? id)
         {
